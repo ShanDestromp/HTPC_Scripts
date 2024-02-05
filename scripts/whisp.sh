bc5e9833fdc8431eade8b452f68c0a705d569306
Shan Destromp <shan.destromp@gmail.com>
2024-02-04 19:33:02 -0500

    Update whisp.sh
    Fixed checks for existing files

--- scripts/whisp.sh
+++ scripts/whisp.sh
@@ -1,9 +1,11 @@
+
 #!/bin/bash
 
 WISP=/app/.venv/bin/whisper
 
 IFS='
 '
+
 FORM="srt"
 LANG="en"
 DEV="cuda"
@@ -22,16 +24,14 @@ then
   EXT="$2"
   for f in ./*."$EXT"
   do
-
     OUT=${f%\.*} #Removes file extension
-    OUT="${OUT//\.\/}" #removes ./
 
-    if [ -f ./$OUT.srt ] # Check if existing srt exists
+    if [ ! -e "$OUT".srt ] && [ ! -e "$OUT"."$LANG".srt ] # Check if existing srt or lang.srt exists
     then
       echo "$f"
       $WISP --output_format $FORM --language $LANG --device $DEV --verbose=$VERB "$f"
     else
-      echo "Existing sub track: $OUT.srt"
+      echo "Existing sub track: $OUT"
     fi
   done
 else
